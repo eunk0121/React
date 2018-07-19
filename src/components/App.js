946ec1aed3ed192ae72e7fc7118394547ef3c823
EunKyoung Lee <eunkyounglee@EunKyoungui-MacBook-Pro.local>
2018-07-19 23:17:36 +0530

    daily work

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import Ract from 'react';
-import contact from './Contact';
+//import는 ES6문법으로 var React = require('react'); 와 같은 의미
+import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+
     render(){
-        return(
-            <h1>Hello</h1>
+
+        return (
+            <div>
+                <h1>Hello React Skeleton!!!!!!</h1>
+                <h2>{this.state.value}</h2>
+                <button onClick={() => { this.setState({value: this.state.value + 1}) }}>CLICK ME</button>
+            </div>
         );
     }
 }
 
-export default App;
+export default App;//다른곳에서 불러서 사용가능하도록 export하는것으로 module.export = App;과 같은 의미
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 
 export default class ContactInfo extends React.Component {
     render(){
